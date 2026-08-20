8710e94c9852840707ec4d9a3d5b86f7f487eb2d
Uav3537 <uav3537@gmail.com>
2026-08-21 07:48:31 +0900

    Test Script Fix

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-import * as esm from "./dist/index.mjs";
-import * as cjs from "./dist/index.js";
+import * as esm from "./dist/index.js";
+import * as cjs from "./dist/index.cjs";
 
 if (!Object.keys(esm).length) throw new Error("ESM broken");
 if (!Object.keys(cjs).length) throw new Error("CJS broken");
